fdf61cb220335f4cc16eb83a6861652118106289
Sumit <sumit.16noida@gmail.com>
2024-10-19 22:21:30 +0530

    remove prod bugs

--- app/components/ui/icon-cloud.tsx
+++ app/components/ui/icon-cloud.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
